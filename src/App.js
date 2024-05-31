b6f022b904ad43bee3727bc1056aed32c7e74c7e
harishcoder-dev <55884172+harishcoder-dev@users.noreply.github.com>
2024-05-31 23:02:28 +0530

    deploy on minikube

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Github Actions Jai
+          Welcome to Github Actions Jai Vignesh
         </p>
         <a
           className="App-link"
